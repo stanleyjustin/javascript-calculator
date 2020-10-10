e42e8302aec1963c55c3b5ba35a0ad154d96a187
stanleyjustin <justinhstanley@gmail.com>
2020-10-10 11:35:26 -0300

    Initial commit with html + css roughed in

---
+++ js/calculator.js
@@ -0,0 +1,24 @@
+//variables to assign inputted numbers to
+let initialDisplay = 0;
+let inputX = 0;
+let inputY = 0;
+let output = 0;
+
+//function to add
+
+
+//function to subtract
+
+//function to multiply
+
+//function to divide
+
+//function to operate one of the above functions on different numbers
+
+//function to change display when you hit number buttons
+// > store the number in a variable called "display value"
+
+//when you hit equal the operator function runs the number and selected operator
+//outputs on the display
+
+//add clear function that resets calculator to "0" the beginning value
