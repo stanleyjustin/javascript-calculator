1fd798d60a6f858dc0d38db64044b3f6a29cd58f
stanleyjustin <justinhstanley@gmail.com>
2020-10-13 20:47:25 -0300

    added javascript to power calculator & added dracula color theme

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,62 @@
-//variables to assign inputted numbers to
-let initialDisplay = 0;
-let inputX = 0;
-let inputY = 0;
-let output = 0;
+// document selectors
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator-keys')
+const display = calculator.querySelector('.calculator-display')
 
-//function to add
+// use one event listener on parent element of the buttons to handle all buttons at once
+keys.addEventListener('click', event => {
+    if (!event.target.closest('button')) return
 
+    const key = event.target
+    const keyValue = key.textContent
+    const displayValue = display.textContent
+    const type = key.dataset.type
+    const {previousKeyType} = calculator.dataset
 
-//function to subtract
+// handle numbers: appear in display
+    if ((type === 'number')) {
+        if (displayValue === '0') {
+        display.textContent = keyValue
+    } else if (previousKeyType === "operator") {
+        display.textContent = keyValue
+    } else {
+        display.textContent = displayValue + keyValue
+    }
+}
 
-//function to multiply
+// handle operators: do not appear in display
+    if (type === 'operator') {
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+        operatorKeys.forEach(el => {el.dataset.state= '' })
+        key.dataset.state = 'selected'
 
-//function to divide
+// store value of first number and operator for later calculation
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = key.dataset.key;
+    }
 
-//function to operate one of the above functions on different numbers
+// perform calculation when hitting equal
+    if (type === 'equal') {
+        const firstNumber = parseInt(calculator.dataset.firstNumber);
+        const operator = calculator.dataset.operator;
+        const secondNumber = parseInt(displayValue);        
 
-//function to change display when you hit number buttons
-// > store the number in a variable called "display value"
+        let result = '';
+        if (operator === 'plus') result = firstNumber + secondNumber;
+        if (operator === 'subtract') result = firstNumber - secondNumber;
+        if (operator === 'times') result = firstNumber * secondNumber;
+        if (operator === 'divided') result = firstNumber / secondNumber;
+        console.log(result)
 
-//when you hit equal the operator function runs the number and selected operator
-//outputs on the display
+        display.textContent = result;
+    }
+// perform reset of dataset numbers affixed to 'calculator' and reset display on AC press
+    if (type === 'clear') {
+        display.textContent = '0';
+        delete calculator.dataset.firstNumber;
+        delete calculator.dataset.secondNumber;
+    }
 
-//add clear function that resets calculator to "0" the beginning value
+    calculator.dataset.previousKeyType = type
+
+})
